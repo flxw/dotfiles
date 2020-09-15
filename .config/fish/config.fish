c87d7e42cba59377b8ca4cb72a3bb585476bf849
Felix Wolff <wolff.felix@protonmail.com>
2020-09-15 22:08:11 +0200

    Tweak configuration for use with i3 on private machine

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,12 @@ export PATH="/opt/local/bin:$PATH"
 export PATH="$HOME/node_modules/.bin:$HOME/Library/Python/3.7/bin:$PATH"
 export PATH="$HOME/.jenv/bin:$PATH"
 
-status --is-interactive; and source (jenv init -|psub)
-
 export PYSPARK_PYTHON=/usr/bin/python3
 export PYSPARK_DRIVER_PYTHON=/usr/bin/python3
+
+# start X upon login
+if status --is-login
+  if systemctl -q is-active graphical.target && ! test $DISPLAY -a $XDG_VTNR -eq 1
+    exec startx
+  end
+end
