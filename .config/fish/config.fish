69e4e0fb1c646c5ce580d9b20442776cfddc44a9
Felix Wolff <wolff.felix@protonmail.com>
2020-09-25 23:14:11 +0200

    Set EDITOR to vim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 export PATH="/opt/local/bin:$PATH"
 export PATH="$HOME/node_modules/.bin:$HOME/Library/Python/3.7/bin:$PATH"
 export PATH="$HOME/.jenv/bin:$PATH"
+export EDITOR="vim"
 
 export PYSPARK_PYTHON=/usr/bin/python3
 export PYSPARK_DRIVER_PYTHON=/usr/bin/python3
